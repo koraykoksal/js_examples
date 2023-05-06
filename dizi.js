3b43a4c8b58cdbae91ca69f98ee3fe28ad854c53
Koray KOKSAL <tkkoksal@icloud.com>
2023-05-06 14:19:17 +0300

    operators

---
+++ dizi.js
@@ -0,0 +1,17 @@
+
+
+let values=["mercedes","bmw","audi"]        //diziler
+
+values[2] = "car"  //values dizisinin 2. indexine ekleme yapar
+values[5] = "bus"   //values dizisinin 5. indexine ekleme yapar
+values[3] = 11      //valeu dizisinin 3. indexine integer bir deger atar
+values[4] = 3.14      //valeu dizisinin 4. indexine float bir deger atar
+
+console.log(values.length)      //values dizisinin eleman sayısını verir.(index sayısı 0 dan başladığı için sıfırdan saymaya başlar)
+console.log(typeof values)      //values dizisinin tipini gösterir
+
+
+
+
+
+
